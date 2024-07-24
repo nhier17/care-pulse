fff8e7c1eb5ee9f1d6de5d4e5831818a0232cae9
Abraham <abrahamnhier97@gmail.com>
2024-07-24 22:04:29 +0300

    register patient

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePluse
+            &copy; {new Date().getFullYear()} CarePulse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
