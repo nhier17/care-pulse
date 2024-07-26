62211d45b5d193c3ebf21e3e1bca20b44181a6ad
Abraham <abrahamnhier97@gmail.com>
2024-07-26 10:50:24 +0300

    admin dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import StatCard from "@/components/StatCard";
+
+const AdminPage = () => {
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+        <Image
+           src="/assets/icons/logo-full.svg"
+           height={32}
+           width={162}
+           alt="logo"
+           className="h-8 w-fit"
+        />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+        <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+        </section>
+
+        <section className="admin-stat">
+        <StatCard
+           type="appointments"
+           count=""
+           label="Scheduled appointments"
+           icon={"/assets/icons/appointments.svg"}
+        />
+        <StatCard
+           type="pending"
+           count=""
+           label="Pending appointments"
+           icon={"/assets/icons/pending.svg"}
+        />
+        <StatCard
+             type="cancelled"
+             count=""
+             label="Cancelled appointments"
+             icon={"/assets/icons/cancelled.svg"}
+        />
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default AdminPage
